36dd034a1aa9f9901bf0238089ac6dbf259e3c2b
Côme Peladeau <come.peladeau@telecom-paris.fr>
2024-01-30 18:10:16 +0100

    Changed filters

--- filters/LR.py
+++ filters/LR.py
@@ -1,9 +1,9 @@
 ### Linkwitz-Riley Filters
-from .base import Filter, Filters_Series
+from .base import Filter, Series
 from . import butterworth
 
 
-class LP4(Filters_Series):
+class LP4(Series):
     def __init__(self, f0, sr=44100):
         super(LP4, self).__init__(
             butterworth.LP2(f0=f0, sr=sr),
@@ -11,7 +11,7 @@ class LP4(Filters_Series):
             sr=sr
         )
 
-class HP4(Filters_Series):
+class HP4(Series):
     def __init__(self, f0, sr=44100):
         super(HP4, self).__init__(
             butterworth.HP2(f0=f0, sr=sr),
--- filters/base.py
+++ filters/base.py
@@ -20,7 +20,7 @@ class Filter():
         return self.process(x)
     
 
-class Filters_Series():
+class Series(Filter):
     def __init__(self, *filters, sr):
         self.filters_list = []
         for filter in filters:
@@ -37,8 +37,21 @@ class Filters_Series():
         for filter in self.filters_list:
             y = filter.process(y)
         return y
+
+class Parallel(Filter):
+    def __init__(self, *filters, sr):
+        self.filters_list = []
+        for filter in filters:
+            self.filters_list.append(filter)
     
-    def get_impulseResponse(self, n):
-        x = np.zeros(n)
-        x[0]=1
-        return self.process(x)
+    def get_rfft(self, n):
+        out = np.zeros(shape=n//2+1, dtype=np.complex128)
+        for filter in self.filters_list:
+            out = out+filter.get_rfft(n=n)
+        return out
+
+    def process(self, x):
+        y = np.zeros_like(x)
+        for filter in self.filters_list:
+            y = filter.process(x)
+        return y
